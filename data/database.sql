1e23454101d44fb22187cddd73f65d1eaa918203
“miguelalonso” <miguel_al_al@hotmail.com>
2022-04-23 14:07:32 +0200

    estructura base

---
+++ data/database.sql
@@ -0,0 +1,45 @@
+CREATE DATABASE hackinions;
+
+USE hackinions;
+
+CREATE TABLE users (
+	id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+    username VARCHAR(20) UNIQUE NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    password TINYTEXT NOT NULL,
+    name VARCHAR(20),
+    last_name VARCHAR(20),
+    bio VARCHAR(250),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE hackentries(
+	id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(100) NOT NULL,
+    content VARCHAR(250) NOT NULL,
+    user_id INT UNSIGNED NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+CREATE TABLE hackvotes(
+    id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    vote BOOLEAN NOT NULL,
+    user_id INT UNSIGNED NOT NULL,
+    hackentries_id INT UNSIGNED NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (hackentries_id) REFERENCES hackentries(id)
+
+);
+
+INSERT INTO users (name,last_name, email, username, bio, password) VALUES 
+("Carlos", "Lopez", "lopez0@google.com", "C.lopez", "Soy Carlos y recojo patatas", SHA2("987654", 512)), 
+("Maria", "Suarez", "suarez0@google.com","A.suarez", "Soy Maria y estoy parada", SHA2("123456", 512)),
+("Carla","Jimenez", "jimenez0@google.com", "C.Jimenez", "Soy Jimenez con J", SHA2("987654", 512));
+
+
+INSERT INTO hackentries(title, content, user_id)
+	VALUES
+    ("precio del gas","El precio del gas esta carísimo, bla bla",2),
+    ("educación infantil", "Los padres de hoy dejan a los niños bla bla",3);
---
+++ helpers.js
@@ -0,0 +1,7 @@
+function generateError(message, code = 500) {
+  const error = new Error(message);
+  error.httpStatus = code;
+  return error;
+}
+
+module.exports = { generateError };
