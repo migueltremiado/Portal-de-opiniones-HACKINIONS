2ac2ced2dff3b0f7234a09c42658e7ec58ce9a8a
Amilcar J. Ruiz <98772743+AmilcarJRuiz@users.noreply.github.com>
2022-04-29 19:50:02 +0200

    Cambio 29.04

--- controllers/users/changeHack.js
+++ controllers/users/changeHack.js
@@ -3,23 +3,9 @@ const { generateError } = require("../../helpers");
 
 async function changeHack(req, res, next) {
   let connection;
-  // //Creamos una variable para utulizar req
-  // let id = req.userId;
-  // //Hacemos un destructuring para llamar a lo que vamos a invocar lo que queremos modificar
-  // let { username, email } = req.body;
-  //await database.user.udpate({ username, email }); ESTO NO SE QUE ES, PERO DA ERROR
 
-  // try {
-  //   await connection.query(
-  //     `
-  //       INSERT INTO changeHack(username, email)      ESTAS INSERTANDO ALGO EN UNA TABLA QUE NO EXISTEM CHANGEHACK NO EXSISTE,
-  //LO QUE HAY QUE HACER ES MODIFICAR EL USER EN LA TABLA USERS
-  //       VALUES(?, ?);
-  //         `,
-  //     [username, email]
-  //   );
   try {
-    //Creamos una variable para utulizar req
+    //Creamos una variable para utilizar req
     let id = req.userId;
     //Hacemos un destructuring para llamar a lo que vamos a invocar lo que queremos modificar
     let { username, email } = req.body;
@@ -35,6 +21,7 @@ async function changeHack(req, res, next) {
           UPDATE users SET username = ?, email = ?
           where id = ?;
             `,
+      //Modificamos la tabla req.userId
       [username, email, req.userId]
     );
     res.send({
